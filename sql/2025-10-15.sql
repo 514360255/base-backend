ba978cf77692327b20abf5c74836320fe7c104cd
guojiuling <514360255@qq.com>
2025-10-16 14:29:22 +0800

    feat: 1、新增专家表 2、医院新增主图、医院环境字段

--- sql/2025-10-15.sql
+++ sql/2025-10-15.sql
@@ -3,10 +3,14 @@
 CREATE TABLE `medical_appointment`.`m_appointment_hospital_expert` (
     `id`                      BIGINT                                               PRIMARY KEY COMMENT '主键',
     `hospital_id`             BIGINT                      NOT NULL                             COMMENT '医院ID',
+    `account_id`              BIGINT                      NOT NULL                             COMMENT '账号ID',
+    `expert_pic`              VARCHAR(255)                NOT NULL                             COMMENT '专家头像',
     `name`                    VARCHAR(50)                 NOT NULL                             COMMENT '姓名',
     `intro`                   VARCHAR(255)                NOT NULL                             COMMENT '简介',
     `domain`                  VARCHAR(255)                NOT NULL                             COMMENT '领域',
-    `title`                   VARCHAR(100)                NOT NULL                             COMMENT '职称'
+    `title`                   VARCHAR(100)                NOT NULL                             COMMENT '职称',
+    `created_at`              DATETIME                    NOT NULL DEFAULT CURRENT_TIMESTAMP   COMMENT '创建时间',
+    `created_by`              VARCHAR(50)                 NOT NULL                             COMMENT '创建人'
 ) COMMENT = '预约医院专家' CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 # 医院表在 `consultation_hours` 字段后新增 `医院介绍主图` 字段
